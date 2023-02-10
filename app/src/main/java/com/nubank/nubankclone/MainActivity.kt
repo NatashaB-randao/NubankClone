4c97e6a3286ad223a4d8a727af0055a2c85a9300
Natasha Brandão <79339286+NatashaB-randao@users.noreply.github.com>
2023-02-10 15:35:32 -0300

    Atualização MainActivity

--- app/src/main/java/com/nubank/nubankclone/MainActivity.kt
+++ app/src/main/java/com/nubank/nubankclone/MainActivity.kt
@@ -2,11 +2,17 @@ package com.nubank.nubankclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nubank.nubankclone.adapter.AdapterPagamento
 import com.nubank.nubankclone.databinding.ActivityMainBinding
+import com.nubank.nubankclone.model.Pagamento
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapterPagamento: AdapterPagamento
+    private val listaPagamento: MutableList<Pagamento> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +20,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar!!.hide()
+
+        val recyclerIconesPagamento = binding.recyclerIconesPagamentos
+        recyclerIconesPagamento.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerIconesPagamento.setHasFixedSize(true)
+        adapterPagamento = AdapterPagamento()
     }
 }
